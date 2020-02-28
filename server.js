4bbe3b213b26d6be2287897410b14d808f781b9a
cristian634 <cristian634@comcast.net>
2020-02-27 19:36:31 -0700

    server post request

--- server.js
+++ server.js
@@ -10,20 +10,59 @@ app.use(express.json());
 
 
 //arrays for tables and wait list
-var currentTables = [];
+var currentTables = [
+    {
+        name: "Test",
+        partysize: "3",
+        phone: "555-555-5555",
+        email: "test@testmail.com"
+
+    }
+];
 
 var waitList = [];
 
 const tableLimit = 5; 
 
+
+//routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"))
 });
 
-app.get("/",(req,res) => {
+app.get("/api/viewtable", (req, res)=> {
+    return res.json(currentTables);
+});
+
+app.get("/viewtable",(req,res) => {
     res.sendFile(path.join(__dirname, "viewtable.html"))
+
+});
+
+app.get("/api/waitlist", (req, res)=>{
+    return res.json(waitList);
 })
+app.get("/waitlist",(req,res)=> {
+    res.sendfile(path.join(__dirname, "waitlist.html"))
+});
 
 
-//routes
 
+app.post("/api/viewtable", function (req,res){
+    var newReservation = req.body;
+
+    console.log(newReservation);
+
+    if (currentTables.length < tableLimit){
+        currentTables.push(newReservation);
+    }
+    else if (currentTables.length >= tableLimit){
+        waitList.push(newReservation);
+    }
+    res.json(newReservation);
+});
+
+
+app.listen(PORT, ()=> {
+    console.log("App listening on PORT: " + PORT);
+})
